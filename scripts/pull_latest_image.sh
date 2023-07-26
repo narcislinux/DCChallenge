e17d6e98185ab94ffde03b79d3a033eba8b74d84
Narges Ahmadi <2179105+narcislinux@users.noreply.github.com>
2023-07-26 12:04:14 +0200

    Update pull_latest_image.sh

--- scripts/pull_latest_image.sh
+++ scripts/pull_latest_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /opt/DCChallenge/.env
+source /opt/DCChallenge/.env 
 DCC_ECR_LOGIN=$(aws ecr get-login-password --region $DCC_DEFAULT_REGION)
 echo $DCC_ECR_LOGIN | docker login -u AWS --password-stdin $DCC_REPOSITORY_URI
 docker pull $DCC_REPOSITORY_URI:latest
